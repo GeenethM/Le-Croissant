6fb9ff8a15f8c7229dd726492d7f569428f63119
GeenethM <geenethmedagedara@gmail.com>
2024-07-03 23:42:14 +0530

    first commit

---
+++ server/index.js
@@ -0,0 +1,23 @@
+const express = require("express")
+const app = express()
+
+
+const main = async () => {
+    try {
+        await app.listen(4000)
+        console.log("server works on port 4000")
+        // console.log("database connected")
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+main();
+
+app.get('/', (req,res) => {
+    try {
+        res.json("wroks")
+    } catch (error) {
+        res.json(error)
+    }
+})
